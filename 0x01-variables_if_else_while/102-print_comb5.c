021a5f7710fb8a3663b3ae303958bc6915d0a73a
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-02-17 07:01:22 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,2 +1,34 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * Write a program that prints all possible combinations of two two-digit numbers.
+ * main - create 2 pairs of numbers that do not repeat
+ * Return: 0
+ */
+ 
+int main(void){
+	int j, i;
+
+	for (j = 0; j <= 99; j++){
+		for (i = j; i <= 99; i++){
+			if (i != j){
+
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+				putchar(' ');
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+
+				if (j * 100 + i != 9899){
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
