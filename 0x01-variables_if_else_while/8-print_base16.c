acdf1e628c2fb30bbef5e95036cd571fcdbc49f6
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-02-17 06:44:50 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,2 +1,20 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void){
+	int j;
+
+	for (j = 48; j < 58; j++){
+		putchar(j);
+  }
+	for (j = 'a'; j <= 'f'; j++){
+		putchar(j);
+  }
+	putchar('\n');
+	return (0);
+}
