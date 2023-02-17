9792ad116581c9e5f9075866cc45f13ae1186bff
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-02-17 18:41:51 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+/* Program prints all possible different combinations of two digits */
 
 /**
- * Write a program that prints all possible different combinations of two digits.
  * main - prints out all the numbers between 00 and 99
  * with no two digits being the same
  * Return: 0
  */
-int main(void){
+int main(void)
+{
 	int i, j;
-
-	for (i = 48; i < 58; i++){
-		for (j = i; j < 58; j++){
-			if (i == j){
+	/* for loop */
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i; j < 58; j++)
+		{
+			if (i == j)
+			{
 				continue;
 			}
 
 			putchar(i);
 			putchar(j);
 
-			if (i == 56 && j == 57){
+			if (i == 56 && j == 57)
+			{
 				break;
 			} else{
 				putchar(',');
