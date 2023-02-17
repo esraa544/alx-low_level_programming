c98b1fefe37da5905e806910c2da1749087fa4cb
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-02-17 06:37:39 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void){
+	int i = 48;
+
+	while (i < 58){
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
