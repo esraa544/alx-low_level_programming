ceb5cabdc5b3347a659be5491761a8df3948d160
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-02-17 17:05:46 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,15 @@
  * main - Entry point
  * Return: 0
  */
-int main(void){
+int main(void)
+{
 	int i;
 
-	for (i = 48; i < 58; i++){
+	for (i = 48; i < 58; i++)
+	{
 		putchar(i);
-		if (i != 57){
+		if (i != 57)
+		{
 			putchar(',');
 			putchar(' ');
 		}
