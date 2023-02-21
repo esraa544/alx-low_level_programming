84f5e38b35bfd084c917dae35157aeb3c8e9e04e
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-02-22 00:03:27 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,17 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+void print_alphabet(void)
+{
+	int lett;
+
+	for (lett = 'a'; lett <= 'z'; lett++)
+	{
+		_putchar(lett);
+	}
+	_putchar('\n');
+}
