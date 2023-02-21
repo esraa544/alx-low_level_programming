4ca1d3711a737cc28cee59f78f37d0a1c60d41af
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-02-21 23:50:21 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,27 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+/*
+ * File: 0-putchar.c
+ * Auth: Brennan D Baraban
+ *
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
