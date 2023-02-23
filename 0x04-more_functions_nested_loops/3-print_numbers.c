82b59e963c6e4e2e5a64731dc5f81b5fe7d659ae
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-02-23 15:48:21 +0200

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
