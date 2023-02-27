10d1a7924f277ff0580b2dc1e3433dc5596c49f9
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-02-28 00:58:53 +0200

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,2 +1,19 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
