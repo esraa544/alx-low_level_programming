8c07d7c57beda760a7cd8a4f4feaa212d0e5005e
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-03-09 00:58:17 +0200

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include "main.h"
+/**
+ * _strlen_recursion - length of string
+ * @s:string
+ * Return:int
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+
+	}
+	return (i);
+}
