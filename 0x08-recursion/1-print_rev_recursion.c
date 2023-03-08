da1783d7c344b4dbe1b52d4509f02598a582964c
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-03-09 00:57:28 +0200

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include "main.h"
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ * Return 0;
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
