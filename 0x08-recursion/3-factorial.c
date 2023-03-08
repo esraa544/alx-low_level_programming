05a391a20a2dd84d0c034a182a1db5e40ad9742a
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-03-09 00:59:02 +0200

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,2 +1,25 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include "main.h"
+/**
+ * factorial - factorial of int
+ * @n:int
+ * Return:int
+ */
+int factorial(int n)
+{
+	int x;
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		x = n * factorial(n - 1);
+	}
+			return (x);
+
+}
