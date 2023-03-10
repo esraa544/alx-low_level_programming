9fba1c23480532d548f743e0a2be7145d402bd5e
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-03-10 23:55:31 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,2 +1,20 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints file name
+ * @argc: first parameter
+ * @argv: second parameter
+ *
+ * Description: a program that prints its name
+ * Return: Always(0) Success
+ */
+
+int main(int argc, char **argv)
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
+}
