5c8cc61106d0505943b0d2eca89f4191ad762857
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-03-22 03:47:18 +0200

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+
+/**
+* main - prints the name ofthe file it was compiled form
+*
+* Return: zero
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
