0791ecf33b532c2d5c3247f485eb1d1b3bf91c30
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-03-24 03:42:46 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include "function_pointers.h"
+
+/**
+* print_name - this function prints a name
+* @name: pointer string
+* @f: pointer to function
+*
+* Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
