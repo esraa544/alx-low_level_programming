263f0b23f546e53ec02938230ddcc409a368ecb7
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-03-31 02:19:50 +0200

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,2 +1,21 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+} /*Author: davidinmichael*/
