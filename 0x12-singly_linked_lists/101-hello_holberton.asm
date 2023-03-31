8c62fcd72e6d26f3a393edab41aa3df7703150af
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-03-31 02:27:42 +0200

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+	global 	main
+	extern 	printf
+
+	section .text
+main:
+	mov	rdi, format
+	mov	rax, 0
+	call	printf
+	mov	rax, 0
+	ret
+format:
+	db "Hello, Holberton", 10, 0
