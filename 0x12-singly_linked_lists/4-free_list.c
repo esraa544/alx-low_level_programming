ec4955cc5453eddc93e6c04c569ea52a804b0ff9
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-03-31 02:24:43 +0200

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,2 +1,21 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the start of the list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *current, *next;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+} /*Author: davidinmichael*/
